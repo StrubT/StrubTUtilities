9f6184d3ac2750f5fc349eb35a5d937b14d40363
Thomas Reto Strub <ths@strubt.ch>
2017-10-23 21:34:30 +0200

    fix #17 for C# 5.0

---
+++ StrubTUtilities/DataExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+using System.Linq;
+
+namespace StrubT {
+
+	static class DataExtensions {
+
+		public static IReadOnlyTable<object> AsReadOnly(this DataTable dataTable) {
+
+			var table = new Table<object>(dataTable.TableName);
+			table.AddColumns(dataTable.Columns.Cast<DataColumn>().Select(c => c.ColumnName).ToList());
+			table.AddRows(dataTable.Rows.Cast<DataRow>().Select(r => r.ItemArray));
+			return table;
+		}
+
+		public static IReadOnlyTable<T> AsReadOnly<T>(this DataTable dataTable) {
+
+			var table = new Table<T>(dataTable.TableName);
+			table.AddColumns(dataTable.Columns.Cast<DataColumn>().Select(c => c.ColumnName).ToList());
+			table.AddRows(dataTable.Rows.Cast<DataRow>().Select(r => dataTable.Columns.Cast<DataColumn>().Select(c => r.Field<T>(c)).ToList()));
+			return table;
+		}
+	}
+}
