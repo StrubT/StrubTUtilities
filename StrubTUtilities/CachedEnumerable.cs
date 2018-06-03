3c35839bdc53fff5c34d638a0d159ffd771af3b0
Thomas Reto Strub <ths@strubt.ch>
2018-06-03 19:36:26 +0200

    add second caching strategy (list)

--- StrubTUtilities/CachedEnumerable.cs
+++ StrubTUtilities/CachedEnumerable.cs
@@ -1,41 +1,96 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace StrubT {
 
 	class CachedEnumerable<T> : IEnumerable<T> {
 
-		IEnumerator<T> enumerator;
-		bool? isEmpty;
-		T current;
-		CachedEnumerable<T> next;
+		const CachingStrategy defaultStrategy = CachingStrategy.SinglyLinkedList;
 
-		public CachedEnumerable() => isEmpty = true;
+		readonly IEnumerable<T> enumerable;
 
-		public CachedEnumerable(IEnumerable<T> enumerable) : this(enumerable.GetEnumerator()) { }
+		public CachedEnumerable() => enumerable = null;
 
-		public CachedEnumerable(IEnumerator<T> enumerator) => this.enumerator = enumerator;
+		public CachedEnumerable(IEnumerable<T> enumerable, CachingStrategy strategy = defaultStrategy) : this(enumerable.GetEnumerator(), strategy) { }
 
-		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+		public CachedEnumerable(IEnumerator<T> enumerator, CachingStrategy strategy = defaultStrategy) {
 
-		public IEnumerator<T> GetEnumerator() {
-
-			var cache = this;
-			while (true) {
-				lock (cache)
-					if (!cache.isEmpty.HasValue && !(cache.isEmpty = !cache.enumerator.MoveNext()).Value) {
-						cache.current = cache.enumerator.Current;
-						cache.next = new CachedEnumerable<T>(cache.enumerator);
-						cache.enumerator = null;
-					}
-
-				if (cache.isEmpty.Value)
-					yield break;
-
-				yield return cache.current;
-				cache = cache.next;
+			switch (strategy) {
+				case CachingStrategy.SinglyLinkedList: enumerable = new Impl_SinglyLinkedList(enumerator); break;
+				case CachingStrategy.ArrayList: enumerable = new Impl_ArrayList(enumerator); break;
+				default: throw new ArgumentException();
 			}
 		}
+
+		IEnumerator IEnumerable.GetEnumerator() => enumerable.GetEnumerator();
+
+		public IEnumerator<T> GetEnumerator() => enumerable.GetEnumerator();
+
+		#region strategy implementations
+
+		class Impl_SinglyLinkedList : IEnumerable<T> {
+
+			readonly object @lock;
+			IEnumerator<T> enumerator;
+			bool? isEmpty;
+			T current;
+			Impl_SinglyLinkedList next;
+
+			//public Impl_SinglyLinkedList(object @lock = null) => (isEmpty, this.@lock) = (true, @lock);
+
+			//public Impl_SinglyLinkedList(IEnumerable<T> enumerable, object @lock = null) : this(enumerable.GetEnumerator(), @lock) { }
+
+			public Impl_SinglyLinkedList(IEnumerator<T> enumerator, object @lock = null) => (this.enumerator, this.@lock) = (enumerator, @lock ?? new object());
+
+			IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+			public IEnumerator<T> GetEnumerator() {
+
+				var cache = this;
+				while (true) {
+					lock (@lock)
+						if (!cache.isEmpty.HasValue && !(cache.isEmpty = !cache.enumerator.MoveNext()).Value) {
+							cache.current = cache.enumerator.Current;
+							cache.next = new Impl_SinglyLinkedList(cache.enumerator, @lock);
+							cache.enumerator = null;
+						}
+
+					if (cache.isEmpty.Value)
+						yield break;
+
+					yield return cache.current;
+					cache = cache.next;
+				}
+			}
+		}
+
+		class Impl_ArrayList : IEnumerable<T> {
+
+			readonly IEnumerator<T> enumerator;
+			readonly List<T> list = new List<T>();
+
+			public Impl_ArrayList(IEnumerator<T> enumerator) => this.enumerator = enumerator;
+
+			IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+			public IEnumerator<T> GetEnumerator() {
+
+				var i = 0;
+				while (true) {
+					lock (enumerator)
+						if (list.Count <= i) {
+							if (!enumerator.MoveNext())
+								yield break;
+
+							list.Add(enumerator.Current);
+						}
+
+					yield return list[i++];
+				}
+			}
+		}
+		#endregion
 	}
 
 	static class CachedEnumerableExtensions {
@@ -43,5 +98,11 @@ namespace StrubT {
 		public static IEnumerable<T> AsCached<T>(this IEnumerable<T> enumerable) => enumerable as CachedEnumerable<T> ?? new CachedEnumerable<T>(enumerable);
 
 		public static IEnumerable<T> AsCached<T>(this IEnumerator<T> enumerator) => new CachedEnumerable<T>(enumerator);
+	}
+
+	public enum CachingStrategy {
+
+		SinglyLinkedList,
+		ArrayList
 	}
 }
